6f80a03d216719103563e9a040829eac347bb596
Aryeh Silver <aryehsilver@hotmail.com>
2022-09-19 22:51:53 +0100

    Use new logo name

--- TheShivisiApp/Helpers/PopTheToast.cs
+++ TheShivisiApp/Helpers/PopTheToast.cs
@@ -2,13 +2,13 @@
 
 namespace TheShivisiApp.Helpers;
 
-  public class PopTheToast {
-    public static void PopIt(string notifText) =>
-      new ToastContentBuilder()
-          .AddText("The Shivisi App")
-          .AddText(!string.IsNullOrWhiteSpace(notifText) ? notifText : "Remember!" + Environment.NewLine + "You're not the one in charge here!")
-          //.AddHeroImage(new Uri("file:///"))
-          .AddAppLogoOverride(new Uri("file:///" + Path.GetFullPath("Data/ShivisiAppNewLogo.png")), ToastGenericAppLogoCrop.Circle)
-          .AddAttributionText("Via TSA")
-          .Show(toast => toast.ExpirationTime = DateTime.Now.AddMinutes(1));
-  }
+public class PopTheToast {
+  public static void PopIt(string notifText) =>
+    new ToastContentBuilder()
+        .AddText("The Shivisi App")
+        .AddText(!string.IsNullOrWhiteSpace(notifText) ? notifText : "Remember!" + Environment.NewLine + "You're not the one in charge here!")
+        //.AddHeroImage(new Uri("file:///"))
+        .AddAppLogoOverride(new Uri("file:///" + Path.GetFullPath("Data/Logo.png")), ToastGenericAppLogoCrop.Circle)
+        .AddAttributionText("Via TSA")
+        .Show(toast => toast.ExpirationTime = DateTime.Now.AddMinutes(1));
+}
